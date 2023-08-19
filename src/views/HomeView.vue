a3d19c14a53e704be51a27ad52806127e4459985
Kasimba666 <kasimba666@gmail.com>
2023-08-19 12:24:08 +0300

    Collection images Modal

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,75 @@
 <template>
   <div class="HomeView">
-    <b-container>
-      <b-row>
-        <div class="col-12">
+<!--    <b-container>-->
+<!--      <b-row>-->
+<!--        <div class="col-12">-->
           <div class="title" @click="getPathRandomImage">
             <div class="title-image-background" :style="{backgroundImage: `url(${titleImg})`}"></div>
           </div>
 
-        </div>
-      </b-row>
-      <b-row>
-        <div class="col-12">
+<!--        </div>-->
+<!--      </b-row>-->
+<!--      <b-row>-->
+<!--        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">-->
           <div class="breeds-list">
-            <div class="breeds-card" v-for="(b, i) of breeds" :key="i">
+            <div class="breeds-card"
+                 v-for="(b, i) of breeds" :key="i"
+                 @click="isOpenBreedModal=true; getPathImagesByBreed(b.name)">
               <div class="breeds-card-image-placeholder">
-                <!--                <b-img  height="140" width="140" rounded onerror = "this.setNoImage(i)" :src=b.img></b-img>-->
                 <div class="breeds-card-image-background" :style="{backgroundImage: `url(${b.img})`}"></div>
-
-                <!--                <div class="breeds-card-no-image">{{ flag == null ? 'no image' : '' }}</div>-->
               </div>
-              <div class="breeds-card-text">
+              <div class="breeds-card-text" :class="{extra: (b.sub.length>0)}">
                 {{ b.name.slice(0, 1).toUpperCase() + b.name.slice(1) }}
               </div>
             </div>
           </div>
+<!--        </div>-->
+        <!--        <div class="col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2">-->
+        <!--          <div class="breeds-list" v-if="!!currentBreed.sub && currentBreed.sub.length > 0">-->
+        <!--            <div class="breeds-card"-->
+        <!--                 v-for="(sb, j) of currentBreed.sub" :key="j">-->
+        <!--              <div class="breeds-card-image-placeholder">-->
+        <!--                <div class="breeds-card-image-background" :style="{backgroundImage: `url(${sb.img})`}"></div>-->
+        <!--              </div>-->
+        <!--              <div class="breeds-card-text">-->
+        <!--                {{ sb.name.slice(0, 1).toUpperCase() + sb.name.slice(1) }}-->
+        <!--              </div>-->
+        <!--            </div>-->
+        <!--          </div>-->
+        <!--        </div>-->
+<!--      </b-row>-->
+<!--    </b-container>-->
+
+    <div class="breed-modal" :style="{display: isOpenBreedModal?'block':'none'}">
+      <b-btn class="modal-btn-close" size="md"
+             variant="danger"
+             @click="isOpenBreedModal=false">
+        &times;
+      </b-btn>
+      <div class="breed-modal-collection-images">
+        <div class="breed-modal-image"
+             v-for="(img, i) of imgs" :key="i"
+             :style="{backgroundImage: `url(${img})`}"
+             @click="isOpenModalFullImage=true; full_img = img"
+        >
         </div>
-      </b-row>
-    </b-container>
+      </div>
+    </div>
+
+    <div class="breed-modal-full-image" :style="{display: isOpenModalFullImage?'block':'none'}">
+      <b-btn class="modal-btn-close" size="md"
+             variant="danger"
+             @click="isOpenModalFullImage=false">
+        &times;
+      </b-btn>
+      <div class="full-image"
+           :style="{backgroundImage: `url(${full_img})`}">
+      </div>
+    </div>
+
+
   </div>
+
 </template>
 
 <script>
@@ -36,10 +78,11 @@ import axios from "axios";
 import $ from 'jquery';
 
 const pathMain = `https://dog.ceo/api/`;
-const pathAllBreeds = `breeds/list/all`;
-const pathSingleRandomImage = `breeds/image/random`;
-const pathImagesByBreed = (breed) => `breed/${breed}/images`;
-const pathImagesByBreedSubbreed = (breed, subbreed) => `breed/${breed}/${subbreed}/images`;
+const pathAllBreeds = pathMain + `breeds/list/all`;
+const pathRandomImage = pathMain + `breeds/image/random`;
+const pathRandomImageByBreed = (breed) => pathMain + `breed/${breed}/images/random`;
+const pathImagesByBreed = (breed) => pathMain + `breed/${breed}/images`;
+const pathImagesByBreedSubbreed = (breed, subbreed) => pathMain + `breed/${breed}/${subbreed}/images`;
 
 export default {
   name: 'HomeView',
@@ -47,20 +90,23 @@ export default {
   data() {
     return {
       breeds: [],
+      imgs: [],
+      full_img: null,
+      currentBreed: null,
       raw_images: null,
       titleImg: null,
       noImage: null,
       isExists: false,
+      isOpenBreedModal: false,
+      isOpenModalFullImage: false,
     }
   },
   methods: {
     onLoad() {
       this.isExists = true;
-
     },
     onError() {
       this.isExists = false;
-
     },
 
     checkImage(imageSrc, good) {
@@ -88,12 +134,12 @@ export default {
 
         $.ajax({
           url: urls[i],
-          type:'HEAD',
-          error: function(){
+          type: 'HEAD',
+          error: function () {
             this.isExists = false;
-            console.log("Error: " + i + " " + urls[i]);
+            // console.log("Error: " + i + " " + urls[i]);
           },
-          success: function(){
+          success: function () {
             this.isExists = true;
           }
         });
@@ -103,7 +149,7 @@ export default {
       } while ((i < urls.length) && !this.isExists && (i < 6));
 
       // if (this.isExists) {
-      console.log("всего " + urls.length + ", номер " + i  + " сейчас отправим: " + this.isExists + "::" + urls[i - 1]);
+      // console.log("всего " + urls.length + ", номер " + i + " сейчас отправим: " + this.isExists + "::" + urls[i - 1]);
 
       return urls[i - 1];
       // return this.isExists ? urls[i - 1] : null;
@@ -113,28 +159,29 @@ export default {
     getAllBreeds() {
       let arr = [];
       let raw = null;
-      axios.get(pathMain + pathAllBreeds)
+      axios.get(pathAllBreeds)
           .then(response => {
             raw = Object.entries(response.data.message);
             if (!!raw && raw.length > 0) {
               raw.forEach((item) => {
                 if (true || ((item[0] === 'segugio') || (item[0] === 'setter'))) {
-
                   let imgValidURL = null;
                   let subs = [];
-                  axios.get(pathMain + pathImagesByBreed(item[0]))
+                  axios.get(pathRandomImageByBreed(item[0]))
                       .then(response => {
                         //вернуть первую действующую ссылку
-                        imgValidURL = this.getValidURL(response.data.message);
-                        console.log(item[0] + ': ' + imgValidURL);
+                        imgValidURL = response.data.message;
+                        // imgValidURL = this.getValidURL(response.data.message);
+                        // console.log(item[0] + ': ' + imgValidURL);
                         // console.log(imgValidURL);
-                        // item[1].forEach(itemSub => {
-                        //   let imgSubValidURL = null;
-                        //   axios.get(pathMain + pathImagesByBreedSubbreed(item[0], itemSub)).then(response => {
-                        //     imgSubValidURL = this.getValidURL(response.data.message);
-                        //     subs.push({name: itemSub, img: imgSubValidURL});
-                        //   }).catch(err => console.log(err));
-                        // });
+                        item[1].forEach(itemSub => {
+                          let imgSubValidURL = null;
+                          axios.get(pathImagesByBreedSubbreed(item[0], itemSub)).then(response => {
+                            // imgSubValidURL = response.data.message;
+                            imgSubValidURL = this.getValidURL(response.data.message);
+                            subs.push({name: itemSub, img: imgSubValidURL});
+                          }).catch(err => console.log(err));
+                        });
                         arr.push({name: item[0], img: imgValidURL, sub: subs});
                       })
                       .catch(err => console.log(err));
@@ -144,14 +191,14 @@ export default {
             }
           })
           .catch(err => console.log(err));
+      arr = arr.sort();
       return arr
-    }
-    ,
+    },
 
     getPathRandomImage() {
-      axios.get(pathMain + pathSingleRandomImage).then(response => this.titleImg = response.data.message).catch(err => console.log(err));
-    }
-    ,
+      axios.get(pathRandomImage).then(response => this.titleImg = response.data.message).catch(err => console.log(err));
+    },
+
 
     init() {
       // const getBreeds = async () => {
@@ -164,8 +211,17 @@ export default {
       // };
       this.getPathRandomImage();
       this.breeds = this.getAllBreeds();
-    }
-    ,
+      // this.setSubbreed(0);
+      // console.log(this.breeds[0].name);
+      // console.log(this.currentBreed.sub);
+    },
+
+    getPathImagesByBreed(bname) {
+      axios.get(pathImagesByBreed(bname)).then(response => {
+        this.imgs = response.data.message;
+      }).catch(err => console.log(err));
+    },
+
   },
   mounted() {
     this.init();
@@ -176,6 +232,8 @@ export default {
 
 <style lang="scss">
 .HomeView {
+  position: relative;
+
   .title {
     flex: 0 0 auto;
     width: 100%;
@@ -207,7 +265,7 @@ export default {
     flex-flow: row wrap;
     justify-content: space-between;
     gap: 5px;
-
+    //border: 1px solid hsla(0, 0%, 0%, 0.2);
 
     .breeds-card {
       width: 150px;
@@ -265,10 +323,84 @@ export default {
         flex-flow: column wrap;
         justify-content: center;
         align-items: center;
+
+        &.extra {
+          font-weight: bold;
+        }
       }
     }
   }
 
+  .modal-btn-close {
+    position: absolute;
+    right: 5px;
+    top: 5px;
+    opacity: 80%;
+    z-index: 10;
+  }
+
+  .breed-modal {
+    position: fixed;
+    left: 5%;
+    top: 0px;
+    width: 90%;
+    height: 90%;
+    height-max: 100%;
+    margin: 1%;
+    background-color: white;
+    border: 1px solid hsla(0, 0%, 50%, 0.8);
+
+
+    .breed-modal-collection-images {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-flow: row wrap;
+      justify-content: space-around;
+      gap: 5px;
+      border: 1px solid hsla(0, 0%, 50%, 0.8);
+      overflow-y: auto;
+      z-index: 5;
+
+      .breed-modal-image {
+        position: relative;
+        width: 100px;
+        height: 100px;
+        background-size: contain;
+        background-position: center;
+        background-repeat: no-repeat;
+
+        &:hover {
+          box-shadow: 0 0 10px 3px rgba(0, 140, 186, 0.5);
+        }
+      }
+
+    }
+  }
+
+  .breed-modal-full-image {
+    position: fixed;
+    left: 5%;
+    top: 0px;
+    width: 90%;
+    height: 90%;
+    height-max: 100%;
+    margin: 1%;
+    background-color: white;
+    border: 1px solid hsla(0, 0%, 50%, 0.8);
+
+    .full-image {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      background-size: auto auto;
+      background-position: center;
+      background-repeat: no-repeat;
+      border: 1px solid hsla(0, 0%, 50%, 0.8);
+    }
+
+  }
 }
 
 </style>
